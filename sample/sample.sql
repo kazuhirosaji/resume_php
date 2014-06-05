cc50152c94888615985d1612d5d15901f78ea96b
Ikuo Degawa <degawa@ikuwow.com>
2014-06-05 17:45:31 +0900

    sample.sqlを追加

---
+++ sample/sample.sql
@@ -0,0 +1,17 @@
+# データベース作成
+CREATE DATABASE IF NOT EXISTS `twitter` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+# tweetsテーブル作成
+CREATE TABLE IF NOT EXISTS `twitter`.`tweets` (
+    `id` INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+    `content` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+    `created` DATETIME NULL DEFAULT NULL
+) ENGINE = INNODB CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+# データベース指定
+USE twitter;
+
+# データ挿入
+INSERT INTO tweets(username, content, created) VALUES ('hasebe', 'Goodfindのセミナーなう',  NOW());
+
